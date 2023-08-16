ed3c57dfb01098243875de29a589f15fd24887b7
Deepika Limbu <skyler13123@gmail.com>
2023-08-16 16:13:51 -0600

    first part of project

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hi");
+const options =["rock", "paper","scissors"];
+
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
